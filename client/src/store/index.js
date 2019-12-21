435191df1077785dd2a3c8409f4c4679f887de2c
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-21 12:46:04 -0700

    adds to state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    bugs: [],
+    activeBug: {},
+    notes: []
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {}
+});
