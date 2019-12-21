1b9165b48cd174f57d5529958ccbe8518c864885
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-21 13:03:15 -0700

    creates home view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app" class="container-fluid">
-    <div class="row d-flex flex-row-reverse">
-      <div id="nav" class="col-3 text-right">
-        <router-link to="/">Report</router-link>
+  <div id="app">
+    <div class="container-fluid">
+      <div class="row d-flex flex-row-reverse">
+        <div id="nav" class="col-3 text-right">
+          <router-link to="/">Report</router-link>
+        </div>
       </div>
     </div>
     <router-view />
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="home">
-
-  </div>
+<main class="home container-fluid">
+  <header class="row">
+    <div class="col-4">
+      <h1>Current Bugs</h1>
+    </div>
+  </header>
+  <body class="row"></body>
+  <footer></footer>
+</main>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home'
-}
+  name: "home"
+};
 </script>
