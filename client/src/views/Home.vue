bb99291a492bbfca4515b8069a7fd982094100dc
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-21 13:22:51 -0700

    expands on home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,7 +5,27 @@
       <h1>Current Bugs</h1>
     </div>
   </header>
-  <body class="row"></body>
+  <body class="row">
+    <div class="col-10 m-auto">
+      <div class="row">
+        <div class="col-3">
+          <h4>Title</h4>
+        </div>
+        <div class="col-3">
+          <h4>Reported By</h4>
+        </div>
+        <div class="col-3">
+          <h4>Status</h4>
+        </div>
+        <div class="col-3">
+          <h4>Last Modified</h4>
+        </div>
+      </div>
+      <div class="row bugs">
+        <div class="col-12"></div>
+      </div>
+    </div>
+  </body>
   <footer></footer>
 </main>
 </template>
