8ae601da3e02dca8b8ffc8287a1965b7c1a10071
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-21 14:46:19 -0700

    adds form for creating bugs

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,7 +7,15 @@
   </header>
   <body class="row">
     <div class="col-12">
-      <form @submit.prevent="createBug"></form>
+      <form @submit.prevent="createBug">
+        <input type="text" v-model="newBug.title" placeholder="bug title..." required />
+        <input type="text" v-model="newBug.name" placeholder="your name..." required />
+        <input type="text" v-model="newBug.title" placeholder="bug title..." required />
+        <div class="form-group">
+          <label for="bug-description"></label>
+          <textarea class="form-control" id="bug-description" rows="3"></textarea>
+        </div>
+      </form>
     </div>
     <div class="col-10 m-auto">
       <div class="row">
@@ -39,6 +47,27 @@ export default {
   name: "bug",
   mounted() {
     this.$store.dispatch("getBugs");
+  },
+  data() {
+    return {
+      newBug: {
+        title: "",
+        reportedBy: "",
+        description: ""
+      }
+    };
+  },
+  methods: {
+    createBug() {
+      let bug = { ...this.newBug };
+      this.$store.dispatch("createBug", bug);
+      this.newBug = {
+        title: "",
+        reportedBy: "",
+        description: "",
+        closed: false
+      };
+    }
   },
   computed: {
     bugs() {
