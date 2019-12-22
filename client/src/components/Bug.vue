251e42e495cb5f57f2ec182493808b17ca7b93c4
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-22 15:31:21 -0700

    building bug component

---
+++ client/src/components/Bug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="car">
+    <div class="col-3">{{ bugData.title }}</div>
+    <div class="col-3">{{ bugData.reportedBy }}</div>
+    <div class="col-3">open</div>
+    <div class="col-3"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bug",
+  props: ["bugData"],
+  methods: {}
+};
+</script>
+
+<style>
+</style>
