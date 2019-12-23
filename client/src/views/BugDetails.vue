451576929f9d8a93b115d0ce6f51ad733ae19c3c
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-23 16:51:28 -0700

    sets up BugDetails view

---
+++ client/src/views/BugDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bug-details container-fluid">
+    <header class="row">
+      <div class="col-12"></div>
+    </header>
+    <main class="row"></main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
