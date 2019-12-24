c4d07e307fc2319b40a262a67445bc2308bb5f78
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-23 17:07:10 -0700

    adds router link to BugDetails, to Bug component

--- client/src/components/Bug.vue
+++ client/src/components/Bug.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bug row">
-    <div class="col-3">{{ bugData.title }}</div>
-    <div class="col-3">{{ bugData.reportedBy }}</div>
-    <div class="col-3">open</div>
-    <div class="col-3">{{ bugData.updatedAt}}</div>
+  <div class="bug">
+    <router-link :to="{name: 'bugDetails', params: { id: bugData.id } }" class="row">
+      <div class="col-3">{{ bugData.title }}</div>
+      <div class="col-3">{{ bugData.reportedBy }}</div>
+      <div class="col-3">open</div>
+      <div class="col-3">{{ bugData.updatedAt}}</div>
+    </router-link>
   </div>
 </template>
 
