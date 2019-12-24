13d067acbc53f6ab190687de3ddf5155c102ec37
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-23 17:17:49 -0700

    connects getBugId to BugDetails view

--- client/src/views/BugDetails.vue
+++ client/src/views/BugDetails.vue
@@ -3,14 +3,18 @@
     <header class="row">
       <div class="col-12"></div>
     </header>
-    <main class="row"></main>
+    <main class="row">
+      <div class="col-12">{{ bug}}</div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
   name: "BugDetails",
-  mounted() {},
+  mounted() {
+    this.$store.dispatch("getBugById", this.$route.params.id);
+  },
   computed: {
     bug() {
       return this.$store.state.activeBug;
