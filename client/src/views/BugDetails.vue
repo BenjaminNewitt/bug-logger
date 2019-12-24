71c6d809cd0ae20039cf9849714ef4332c03cf91
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-24 10:07:43 -0700

    adds tp BugDetails

--- client/src/views/BugDetails.vue
+++ client/src/views/BugDetails.vue
@@ -4,7 +4,19 @@
       <div class="col-12"></div>
     </header>
     <main class="row">
-      <div class="col-12">{{ bug}}</div>
+      <div class="col-10 mx-auto">
+        <div class="row">
+          <div class="col-12">
+            <p>Title</p>
+            <h1>{{bug.title}}</h1>
+            <p>Reported by</p>
+            <h2>{{bug.reportedBy}}</h2>
+          </div>
+          <div class="col-12">
+            <p>{{ bug.description }}</p>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
