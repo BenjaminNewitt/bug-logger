89f32681fdf00c7eac49efb1e834a27b8ba0d71d
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-12-29 15:25:55 -0700

    updates Bug component to show status of bug

--- client/src/components/Bug.vue
+++ client/src/components/Bug.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="bug">
     <router-link :to="{name: 'bugDetails', params: { id: bugData.id } }" class="row">
-      <div class="col-3">{{ bugData.title }}</div>
-      <div class="col-3">{{ bugData.reportedBy }}</div>
-      <div class="col-3">open</div>
+      <div class="col-3">
+        <p>{{ bugData.title }}</p>
+      </div>
+      <div class="col-3">
+        <p>{{ bugData.reportedBy }}</p>
+      </div>
+      <div class="col-3">
+        <p v-if="bugData.closed === false">open</p>
+        <p v-if="bugData.closed === true">closed</p>
+      </div>
       <div class="col-3">{{ bugData.updatedAt}}</div>
     </router-link>
   </div>
