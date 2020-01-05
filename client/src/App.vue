b56c5c64609ff18220981960ce6a6f284a232823
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-01-05 15:46:28 -0700

    Styles navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <div class="container-fluid">
-      <div class="row d-flex flex-row-reverse bg-success">
+      <div class="row d-flex flex-row justify-content-between bg-success">
+        <div class="col-3 text-white">Bug Hunter</div>
         <div id="nav" class="col-3 text-right">
-          <router-link to="/" class="text-white">Report</router-link>
+          <router-link to="/" class="text-white">Home</router-link>
         </div>
       </div>
     </div>
