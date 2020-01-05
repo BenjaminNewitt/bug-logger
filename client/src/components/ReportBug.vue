4796294cdb97041a1199a00be8e1ef8ce8fe9bbb
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-01-05 16:03:13 -0700

    expands upon reportBug component

--- client/src/components/ReportBug.vue
+++ client/src/components/ReportBug.vue
@@ -1,4 +1,14 @@
-<template></template>
+<template>
+  <div class="modal" id="report-bug" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2>Report Bug</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
